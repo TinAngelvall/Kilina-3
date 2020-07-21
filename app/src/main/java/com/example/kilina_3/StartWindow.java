40f5de519e9bd4ac30efdb6d4e7c84b8e1ceccd8
Valentina Kilina <angelvall@bk.ru>
2020-07-21 23:55:36 +0500

    Initial commit

---
+++ app/src/main/java/com/example/kilina_3/StartWindow.java
@@ -0,0 +1,14 @@
+package com.example.kilina_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_window);
+    }
+}
